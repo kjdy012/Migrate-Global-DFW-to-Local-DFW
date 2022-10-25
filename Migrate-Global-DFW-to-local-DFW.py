bef92ab45294a29272b1eb4b19fc425fae5c1bb1
kjdy012 <kjdy012@gmail.com>
2022-10-25 14:25:23 -0400

    Update Migrate-Global-DFW-to-local-DFW.py

--- Migrate-Global-DFW-to-local-DFW.py
+++ Migrate-Global-DFW-to-local-DFW.py
@@ -1,30 +1,6 @@
 import json
 import requests
 
-def dict_replace_value(d, old, new):
-    x = {}
-    for k, v in d.items():
-        if isinstance(v, dict):
-            v = dict_replace_value(v, old, new)
-        elif isinstance(v, list):
-            v = list_replace_value(v, old, new)
-        elif isinstance(v, str):
-            v = v.replace(old, new)
-        x[k] = v
-    return x
-
-def list_replace_value(l, old, new):
-    x = []
-    for e in l:
-        if isinstance(e, list):
-            e = list_replace_value(e, old, new)
-        elif isinstance(e, dict):
-            e = dict_replace_value(e, old, new)
-        elif isinstance(e, str):
-            e = e.replace(old, new)
-        x.append(e)
-    return x
-
 #prepare the http connection to NSX Manager
 session = requests.Session()
 session.verify = False
@@ -58,34 +34,36 @@ while profiles_json["children"]:
         user_defined_profiles.append(childprofile)
 
 #Collect DFW configuration
-dfw_path = '/global-manager/api/v1/global-infra?filter=Type-Domain|Group|SecurityPolicy|Rule'
+dfw_path = '/global-manager/api/v1/global-infra?type_filter=Group;SecurityPolicy'
 dfw_json = session.get(gm_nsx_mgr + dfw_path).json()
 
 user_defined_policies = []
 
-for x in dfw_json["children"]:
-	if x["id"] == "default":
-		for y in x["Domain"]["children"]:
-			if list(y.keys())[0] == "SecurityPolicy" and y[list(y.keys())[0]]["id"]  == "Default":
-				continue
-			elif y[(list(y.keys())[0])]["_system_owned"] == False:
-				user_defined_policies.append(y)
-
-
-#Get Local Manger's infra and default domain
-lm_infra_path = '/policy/api/v1/infra?filter=Type-Domain'
-lm_infra_json = session.get(lm_nsx_mgr + lm_infra_path).json()
-
-# Put the Groups, Rules, and Security policy from global manager default domain to local 
-lm_infra_json["children"][0]["Domain"]["children"] = user_defined_policies
+for x in dfw_json["children"][0]["Domain"]["children"]:
+	if list(x.keys())[0] == "SecurityPolicy" and x[list(x.keys())[0]]["id"]  == "Default":
+		continue
+	elif x[(list(x.keys())[0])]["_system_owned"] == False:
+		user_defined_policies.append(x)
 
 #Add User-Defined Services and Profiles to the DFW Configuration
-new_infra_children = lm_infra_json["children"] + user_defined_services + user_defined_profiles
+new_infra = {
+	"resource_type": "Infra",
+	"children": [
+		{
+			"resource_type": "ChildDomain",
+			"Domain": {
+				"resource_type": "Domain",
+				"id": "default",
+				"children": user_defined_policies
+			}
+		}
+	]
+}
 
-lm_infra_json["children"] = new_infra_children
+new_infra["children"] += user_defined_services + user_defined_profiles
 
-lm_dfw_json = dict_replace_value(lm_infra_json, 'global-infra', 'infra')
+new_infra = json.loads(json.dumps(new_infra).replace('global-infra', 'infra'))
 
 #Write the DFW Configuration to a JSON file that can be applied via a PATCH API call to the infra URI: '/policy/api/v1/infra'
 with open("lm_dfw.json", "w") as write_file:
-    json.dump(lm_dfw_json, write_file, indent = 4)
+    json.dump(new_infra, write_file, indent = 4)
